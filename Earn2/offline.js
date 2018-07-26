eccd7ca69b9a4b05e29b7991f8149d3b01a82120
João Pedro <pedro.pedro191@gmail.com>
2018-07-26 16:56:53 -0300

    h

---
+++ Earn2/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1532634967,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/cliente1-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite5-sheet1.png",
+		"images/sprite6-sheet0.png",
+		"images/mesa-sheet0.png",
+		"images/sensorcriarcliente-sheet0.png",
+		"images/mudardirec-sheet0.png",
+		"images/cliente2-sheet0.png",
+		"images/cliente3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
