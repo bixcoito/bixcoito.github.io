9c7aa28bd35a9f36557fc872a1c4370a95a0ba57
João Pedro <pedro.pedro191@gmail.com>
2018-08-15 16:36:22 -0300

    a

--- Earn/offline.js
+++ Earn/offline.js
@@ -1,9 +1,5 @@
 ﻿{
-<<<<<<< HEAD
-	"version": 1534360781,
-=======
-	"version": 1532631260,
->>>>>>> 1f344382feb97187688906bd038065ae9f3e6e99
+	"version": 1534361699,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -17,17 +13,8 @@
 		"images/bback-sheet0.png",
 		"images/spersonagem-sheet0.png",
 		"images/buttonlevel-sheet0.png",
-<<<<<<< HEAD
 		"images/caixadialogo-sheet0.png",
 		"images/backgroundcozinha-sheet0.png",
-=======
-		"images/iconchocolate-sheet0.png",
-		"images/iconcoin-sheet0.png",
-		"images/spritefont.png",
-		"images/sprite3-sheet0.png",
-		"images/animaçãocozinhando-sheet0.png",
-		"images/animaçãocozinhando-sheet1.png",
->>>>>>> 1f344382feb97187688906bd038065ae9f3e6e99
 		"images/bconfig-sheet0.png",
 		"images/bconfig-sheet1.png",
 		"images/sconcorrência-sheet0.png",
@@ -41,7 +28,6 @@
 		"images/clientes-sheet0.png",
 		"images/sprite5-sheet0.png",
 		"images/sprite5-sheet1.png",
-<<<<<<< HEAD
 		"images/emoticon-sheet0.png",
 		"images/barraqualidade-sheet0.png",
 		"images/sprite3-sheet0.png",
@@ -53,14 +39,11 @@
 		"images/bproduzirchocolate-sheet2.png",
 		"images/sprite-sheet0.png",
 		"images/sendorcontadordequalidade-sheet0.png",
-=======
->>>>>>> 1f344382feb97187688906bd038065ae9f3e6e99
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png",
-		"quest.xml"
+		"loading-logo.png"
 	]
 }
